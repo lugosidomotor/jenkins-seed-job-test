c24ae83bb19440262ae54c609ce03febefc3e64b
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-24 01:49:06 +0100

    Update helloWorld.groovy

--- jobs/helloWorld.groovy
+++ jobs/helloWorld.groovy
@@ -1,4 +1,6 @@
-// Assuming branchName is passed from the seed job
+// example.groovy
+def branchName = BRANCH_NAME ?: 'main' // Default to 'main' if BRANCH_NAME is not provided
+
 job("example-${branchName}") {
   steps {
     shell('echo Hello World!')
