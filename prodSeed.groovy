172ab7089205b8fab38f90365bb3b55e8196cb87
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-24 03:28:14 +0100

    Update prodSeed.groovy

--- prodSeed.groovy
+++ prodSeed.groovy
@@ -1,12 +1,8 @@
-folder('seed') {
-  description('Folder for development branch jobs')
-}
-
 job('seed/seed_job_from_main') {
   parameters {
     stringParam('BRANCH_NAME', 'dev', 'A branch neve, amiből építeni fogunk.')
   }
-  
+
   scm {
     git {
       remote {
@@ -15,14 +11,24 @@ job('seed/seed_job_from_main') {
       branches('${BRANCH_NAME}')
     }
   }
-  
+
   steps {
+    // Ellenőrizd az aktuális munkakönyvtárat és a workspace-t
     shell('pwd')
-    shell('ls -la')
+    shell('echo Workspace directory: ${WORKSPACE}')
+
+    // A 'jobs' mappa ellenőrzése
+    shell('if [ -d "${WORKSPACE}/jobs" ]; then echo "jobs directory found"; else echo "jobs directory NOT found"; fi')
+
+    // Job DSL script
     jobDsl {
       scriptText '''
-        def branchName = "${BRANCH_NAME}"
-        def scriptDirectory = new File('jobs')
+        // A Jenkins Job DSL script részei...
+
+        def workspace = new File("${WORKSPACE}")
+        def scriptDirectory = new File(workspace, 'jobs')
+        println("Script directory: " + scriptDirectory.getAbsolutePath())
+
         if (scriptDirectory.exists()) {
             scriptDirectory.eachFile { file ->
                 if (file.name.endsWith('.groovy')) {
@@ -30,7 +36,7 @@ job('seed/seed_job_from_main') {
                 }
             }
         } else {
-            println("The 'jobs' directory was not found. Current directory: " + new File(".").getAbsolutePath())
+            println("The 'jobs' directory was not found in the workspace.")
         }
       '''
     }
