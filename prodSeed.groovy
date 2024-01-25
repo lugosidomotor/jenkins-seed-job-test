77ab12690fa75ae2690f2cb75d53a0eb17ba5b61
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-26 00:49:22 +0100

    Update prodSeed.groovy

--- prodSeed.groovy
+++ prodSeed.groovy
@@ -1,50 +1,22 @@
-job('seed/seed_job_from_main') {
+job('seed-from-dev') {
     parameters {
-        stringParam('BRANCH_NAME', 'dev', 'A branch neve, amiből építeni fogunk.')
-        // Additional parameters can be added here if needed
+        stringParam('BRANCH_NAME', 'default-branch', 'The name of the branch to seed from')
     }
 
     scm {
         git {
             remote {
                 url 'https://github.com/lugosidomotor/jenkins-seed-job-test'
+                credentials('your-credentials-id')
             }
-            branches('${BRANCH_NAME}')
+            branch '${BRANCH_NAME}'
         }
     }
 
     steps {
-        // Checking the current working directory and workspace
-        shell('pwd')
-        shell('echo Workspace directory: ${WORKSPACE}')
-
-        // Checking for the 'jobs' directory
-        shell('if [ -d "${WORKSPACE}/jobs" ]; then echo "jobs directory found"; else echo "jobs directory NOT found"; fi')
-
-        // Job DSL script
-        jobDsl {
-            scriptText '''
-                def branchName = BRANCH_NAME // This variable comes from the job parameters
-
-                // Setting up the workspace and script directory
-                def workspace = new File("${WORKSPACE}")
-                def scriptDirectory = new File(workspace, 'jobs')
-                println("Script directory: " + scriptDirectory.getAbsolutePath())
-
-                // Checking and evaluating groovy scripts in the 'jobs' directory
-                if (scriptDirectory.exists()) {
-                    scriptDirectory.eachFile { file ->
-                        if (file.name.endsWith('.groovy')) {
-                            // Passing the 'branchName' variable to the evaluated script
-                            def script = file.text
-                            def additionalParameters = [branchName: branchName]
-                            evaluate(script, new Binding(additionalParameters))
-                        }
-                    }
-                } else {
-                    println("The 'jobs' directory was not found in the workspace.")
-                }
-            '''
+        dsl {
+            external('jobs/*.groovy')
         }
     }
 }
+
