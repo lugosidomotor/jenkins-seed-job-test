19fc064b3106576c5d90c9157e5fe50f2a0ecef3
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-25 11:59:45 +0100

    Update devSeed.groovy

--- devSeed.groovy
+++ devSeed.groovy
@@ -12,15 +12,15 @@ job('seed/seed_job_from_dev') {
       remote {
         url 'https://github.com/lugosidomotor/jenkins-seed-job-test'
       }
-      // Directly use the parameter name
-      branches("${BRANCH_NAME}")
+      // Use environment variable for the branch name
+      branches("*/${env.BRANCH_NAME}")
     }
   }
   
   steps {
     jobDsl {
-      // Use the parameter directly
-      def branchName = BRANCH_NAME
+      // Use environment variable for the branch name
+      def branchName = env.BRANCH_NAME
       targets '*jobs/*.groovy'
       // Correct syntax for map assignment in Groovy
       additionalParameters([branchName: branchName])
