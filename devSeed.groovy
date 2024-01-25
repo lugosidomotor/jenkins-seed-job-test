c362126922e7080a89c74dcb7e36257642700eee
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-25 12:25:10 +0100

    Update devSeed.groovy

--- devSeed.groovy
+++ devSeed.groovy
@@ -1,24 +1,19 @@
-folder('seed') {
-  description('Folder for development branch jobs')
-}
-
-job('seed/seed_job_from_dev') {
+job('SeedJob') {
+  parameters {
+      stringParam('BRANCH_NAME', 'main', 'Branch to use for seeding jobs')
   }
-  
-  steps {
-    scm {
-      git {
-        remote {
-          url 'https://github.com/lugosidomotor/jenkins-seed-job-test'
-        }
-        // Use the variable
-        branches("dev")
-      }
-    }
 
-    jobDsl {
-      additionalParameters([branchName: "dev"])
-      targets '*jobs/*.groovy'
-    }
+  scm {
+      git {
+          remote {
+              url 'https://github.com/lugosidomotor/jenkins-seed-job-test'
+          }
+          branch('$BRANCH_NAME')
+      }
+  }
+  steps {
+      dsl {
+          external('jobs/*.groovy', [BRANCH_NAME: '$BRANCH_NAME'])
+      }
   }
 }
