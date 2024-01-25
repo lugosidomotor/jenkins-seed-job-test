c3a70e983b45304ec3b6489a80de63db24bab803
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-25 11:33:04 +0100

    Update prodSeed.groovy

--- prodSeed.groovy
+++ prodSeed.groovy
@@ -1,47 +1,50 @@
 job('seed/seed_job_from_main') {
-  parameters {
-    stringParam('BRANCH_NAME', 'dev', 'A branch neve, amiből építeni fogunk.')
-  }
-
-  scm {
-    git {
-      remote {
-        url 'https://github.com/lugosidomotor/jenkins-seed-job-test'
-      }
-      branches('${BRANCH_NAME}')
+    parameters {
+        stringParam('BRANCH_NAME', 'dev', 'A branch neve, amiből építeni fogunk.')
+        // Additional parameters can be added here if needed
     }
-  }
 
-  steps {
-    // Ellenőrizd az aktuális munkakönyvtárat és a workspace-t
-    shell('pwd')
-    shell('echo Workspace directory: ${WORKSPACE}')
-
-    // A 'jobs' mappa ellenőrzése
-    shell('if [ -d "${WORKSPACE}/jobs" ]; then echo "jobs directory found"; else echo "jobs directory NOT found"; fi')
-
-    // Job DSL script
-    jobDsl {
-      scriptText '''
-        def branchName = BRANCH_NAME // Ez a változó a job paramétereiből származik
-
-        def workspace = new File("${WORKSPACE}")
-        def scriptDirectory = new File(workspace, 'jobs')
-        println("Script directory: " + scriptDirectory.getAbsolutePath())
-
-        if (scriptDirectory.exists()) {
-            scriptDirectory.eachFile { file ->
-                if (file.name.endsWith('.groovy')) {
-                    // Átadás a 'branchName' változót az értékelt scriptnek
-                    def script = file.text
-                    def binding = new Binding([branchName: branchName])
-                    evaluate(script, binding)
-                }
+    scm {
+        git {
+            remote {
+                url 'https://github.com/lugosidomotor/jenkins-seed-job-test'
             }
-        } else {
-            println("The 'jobs' directory was not found in the workspace.")
+            branches('${BRANCH_NAME}')
         }
-      '''
     }
-  }
+
+    steps {
+        // Checking the current working directory and workspace
+        shell('pwd')
+        shell('echo Workspace directory: ${WORKSPACE}')
+
+        // Checking for the 'jobs' directory
+        shell('if [ -d "${WORKSPACE}/jobs" ]; then echo "jobs directory found"; else echo "jobs directory NOT found"; fi')
+
+        // Job DSL script
+        jobDsl {
+            scriptText '''
+                def branchName = BRANCH_NAME // This variable comes from the job parameters
+
+                // Setting up the workspace and script directory
+                def workspace = new File("${WORKSPACE}")
+                def scriptDirectory = new File(workspace, 'jobs')
+                println("Script directory: " + scriptDirectory.getAbsolutePath())
+
+                // Checking and evaluating groovy scripts in the 'jobs' directory
+                if (scriptDirectory.exists()) {
+                    scriptDirectory.eachFile { file ->
+                        if (file.name.endsWith('.groovy')) {
+                            // Passing the 'branchName' variable to the evaluated script
+                            def script = file.text
+                            def additionalParameters = [branchName: branchName]
+                            evaluate(script, new Binding(additionalParameters))
+                        }
+                    }
+                } else {
+                    println("The 'jobs' directory was not found in the workspace.")
+                }
+            '''
+        }
+    }
 }
