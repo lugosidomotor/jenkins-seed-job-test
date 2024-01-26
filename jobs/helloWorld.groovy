8aa1298788ed8ba53fd058360a8c52135d1da3fe
Dömötör D. Lugosi <lugalaci@gmail.com>
2024-01-26 01:37:13 +0100

    Update helloWorld.groovy

--- jobs/helloWorld.groovy
+++ jobs/helloWorld.groovy
@@ -1,6 +1,8 @@
 // Example of a DSL script (e.g., example.groovy)
 
-job("helloWorld-${BRANCH_NAME}") {
+def jobPrefix = params.FOLDER ? "${params.FOLDER}/" : ""
+
+job("${jobPrefix}helloWorld-${BRANCH_NAME}") {
     steps {
         shell("echo Building from branch: ${BRANCH_NAME}")
         shell("echo Hello World!")
